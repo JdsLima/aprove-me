27b643bb758f2eeb51b15bdbcd6ee61558924537
JdsLima <jadsonmath874@gmail.com>
2024-11-18 15:29:28 -0400

    feat - add welcome home screen

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import { Aside } from "@/components/Aside";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen p-4 sm:ml-64">
+        <p className="text-2xl text-gray-900 font-bold">Bem vindo ao sistema de gestão de recebíveis</p>
+    </div>
+  );
+}
