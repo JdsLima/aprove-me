b551ba74cbdd12e3527a6497d8ed8778f0952d8a
JdsLima <jadsonmath874@gmail.com>
2024-11-18 15:35:14 -0400

    refactor - adding programming logic to authenticate the user

--- frontend/src/components/LoginForm/index.tsx
+++ frontend/src/components/LoginForm/index.tsx
@@ -1,22 +1,57 @@
+import api from '@/services/api';
 import { useState } from 'react';
+import { CustomToast } from '../CustomToast';
+import { useRouter } from 'next/navigation';
+import { Formik } from 'formik';
 
 export const LoginForm = () => {
+  const router = useRouter();
   const [formData, setFormData] = useState({
-    email: '',
+    username: '',
     password: ''
   });
+  const [isToastOpen, setIsToastOpen] = useState(false);
+  const [toastTitle, setToastTitle] = useState('');
+  const [toastText, setToastText] = useState('');
 
+  // Handle input changes
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData);
+  // Validate login form
+  const validateLogin = (values: typeof formData) => {
+    const errors: Partial<typeof formData> = {};
+    if (!values.username) errors.username = 'Usuário é obrigatório';
+    if (!values.password) errors.password = 'Senha é obrigatória';
+    return errors;
+  };
+
+  // Handle form submit
+  const handleSubmit = async (values: typeof formData) => {
+    try {
+      const response = await api.post('/auth', values);
+      console.log(response.data);
+      // Save token in localStorage
+      localStorage.setItem('bankme_token', response.data.access_token);
+      // Redirect to home page
+      router.push('/home');
+    } catch (error) {
+      console.error(error);
+      setIsToastOpen(true);
+      setToastTitle('Erro');
+      setToastText('Usuário ou senha inválidos');
+    }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <CustomToast 
+        title={toastTitle} 
+        text={toastText} 
+        isOpen={isToastOpen} 
+        onClose={() => setIsToastOpen(false)} 
+      />
       {/* Main Card Container */}
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -35,57 +70,74 @@ export const LoginForm = () => {
           </h1>
 
           {/* Sign In Form */}
-          <form className="space-y-4 md:space-y-6" action="#">
-            {/* Email Input */}
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Usuário
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="aprovame"
-                required
-              />
-            </div>
+          <Formik
+            initialValues={formData}
+            onSubmit={handleSubmit}
+            validate={validateLogin}
+          >
+            {({ values, handleChange, errors }) => (
+              <>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit(values);
+                  }}
+                  className="space-y-4 md:space-y-6"
+                >
+                  {/* Email Input */}
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Usuário
+                    </label>
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      value={values.username}
+                      onChange={handleChange}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="aprovame"
+                      required
+                    />
+                    {errors.username && <p className="text-red-500">{errors.username}</p>}
+                  </div>
 
-            {/* Password Input */}
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Senha
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="aprovame"
-                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
+                  {/* Password Input */}
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Senha
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      placeholder="aprovame"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                    />
+                    {errors.password && <p className="text-red-500">{errors.password}</p>}
+                  </div>
 
-            {/* Submit Button */}
-            <button
-              type="submit"
-              onClick={(e) => handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>)}
-              disabled={!formData.email || !formData.password}
-              className="w-full text-white bg-blue-600 hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Entrar
-            </button>
-          </form>
+                  {/* Submit Button */}
+                  <button
+                    type="submit"
+                    disabled={!values.username || !values.password}
+                    className="w-full text-white bg-blue-600 hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Entrar
+                    </button>
+                </form>
+              </>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
